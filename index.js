199599d3d85d7f7bb09302d808852ffe087a6a9d
Kevin Huang <yellow.khaos@gmail.com>
2018-10-08 15:43:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,6 @@
+function boardMember (name, homeState, training) {
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+  this.veto = function() {return "No, I must disagree"}
+}
