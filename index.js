6f4fbbcaefc15f046487a75c37249bfe27c2d297
Kevin Huang <yellow.khaos@gmail.com>
2018-10-08 15:44:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ function BoardMember (name, homeState, training) {
   this.homeState = homeState
   this.training = training
   this.veto = function() {return "No, I must disagree"}
+  this.approve = function() {return "You can do that!"}
+  this.doCharity = function() {return "I like to help people"}
+  this.releasePressStatement = function() {return "You will see great things from Schuber."}
 }
